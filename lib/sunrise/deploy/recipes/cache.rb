cb0abab0b95b185194275076be7554726b9179df
Igor Galeta <galeta.igor@gmail.com>
2013-02-28 13:14:39 +0200

    Use array flatten rather than wrap

--- lib/sunrise/deploy/recipes/cache.rb
+++ lib/sunrise/deploy/recipes/cache.rb
@@ -5,7 +5,7 @@ module Capistrano
     namespace :cache do
       desc "Clear all cache in project by 'cache_paths': (tmp/cache, public/cache)"
       task :clear do
-        Array.wrap(cache_paths).each do |folder|
+        [cache_paths].flatten.each do |folder|
           path = File.join(deploy_to, folder, "*")
           run "#{try_sudo} rm -rf #{path}"
         end
